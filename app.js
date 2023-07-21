3c2548b6733baa9590259081bf32f1b44590805d
Oksana Bykova <ishkulova.oksana1@yandex.ru>
2023-07-21 14:49:18 +0300

    вернуть порт 3000

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const cors = require('./middlwares/cors');
 
 const { requestLogger, errorLogger } = require('./middlwares/logger');
 
-const { PORT = 3001, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 mongoose.connect(DB_URL, {
   useNewUrlParser: true,
